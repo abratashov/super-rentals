a1236998baae47fd83ae85d72c0dfeb0fc54fd9c
Oleksandr Bratashov <abratashov@gmail.com>
2017-08-27 16:00:29 +0300

    added pages About, Contacts and rental list, also default style and mock api server for tests

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -3,28 +3,42 @@ import moduleForAcceptance from 'super-rentals/tests/helpers/module-for-acceptan
 
 moduleForAcceptance('Acceptance | list rentals');
 
-// test('should show rentals as the home page', function (assert) {
-// });
+test('should show rentals as the home page', function (assert) {
+});
 
-// test('should link to information about the company.', function (assert) {
-// });
+test('should link to information about the company.', function (assert) {
+  visit('/');
+  click('a:contains("About")');
+  andThen(function() {
+    assert.equal(currentURL(), '/about', 'should navigate to about');
+  });
+});
 
-// test('should link to contact information.', function (assert) {
-// });
+test('should link to contact information.', function (assert) {
+  visit('/');
+  click('a:contains("Contact")');
+  andThen(function() {
+    assert.equal(currentURL(), '/contact', 'should navigate to contact');
+  });
+});
 
-// test('should list available rentals.', function (assert) {
-// });
+test('should list available rentals.', function (assert) {
+  visit('/');
+  andThen(function() {
+    assert.equal(find('.listing').length, 3, 'should see 3 listings');
+  });
+});
 
-// test('should filter the list of rentals by city.', function (assert) {
-// });
+test('should filter the list of rentals by city.', function (assert) {
+});
 
-// test('should show details for a selected rental', function (assert) {
-// });
+test('should show details for a selected rental', function (assert) {
+});
 
 test('visiting /', function(assert) {
   visit('/');
 
   andThen(function() {
-    assert.equal(currentURL(), '/');
+    assert.equal(currentURL(), '/rentals', 'should redirect automatically');
   });
 });
